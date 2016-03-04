04df8d258031e835360ac2a1baf42cea848156ce
isaiga0101 <gayfieldfamily@hotmail.com>
2016-03-03 20:22:00 -0800

    Add init_tx function.

--- main.cpp
+++ main.cpp
@@ -53,8 +53,23 @@ void serial328::init_rx(unsigned char n_clock)
 	//UCSR0C default will work here also
 	//UCSR0C |= (1<<UCSZ01) | (1<<UCSZ00);		//8 bits, No parity, 2 stop bits
 	return;
-	return;
 }
+//----------------------------------------------------------------------------
+// Initialize USART to transmit
+void serial328::init_tx(unsigned char n_clock)
+{
+		UBRR0H = 0;
+		UBRR0L = n_clock;		// Set baud rate to___. See Define List
+		
+		UCSR0A |= (1<<U2X0);	//1=double clock speed on
+		UCSR0B |= (1<<TXEN0);	//Transmit on
+		
+		//UCSR0C default will work here also
+		//UCSR0C |= (1<<UCSZ01) | (1<<UCSZ00);		//8 bits, No parity, 2 stop bits
+		return;
+}
+
+
 int main(void)
 {
     /* Replace with your application code */
