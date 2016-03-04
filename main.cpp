d000d2583fe822576d35ee62a068f74cbf717e3d
isaiga0101 <gayfieldfamily@hotmail.com>
2016-03-03 20:17:32 -0800

    Add init_rx function.

--- main.cpp
+++ main.cpp
@@ -37,6 +37,23 @@ void serial328::tx(unsigned char value)		//Transmit Char
 void serial328::rx()
 {
 	while ((UCSR0A & (1<<RXC0))==0);		//Wait for receive ready.
+	char value = UDR0;
+	return(value);
+}
+//---------------------------------------------------------------------------
+// Initialize USART to receive
+void serial328::init_rx(unsigned char n_clock)
+{
+	UBRR0H = 0;
+	UBRR0L = n_clock;		// Set baud rate to___. See Define List
+	
+	UCSR0A |= (1<<U2X0);	//1=double clock speed on
+	UCSR0B |= (1<<RXEN0);	//Receive on
+	
+	//UCSR0C default will work here also
+	//UCSR0C |= (1<<UCSZ01) | (1<<UCSZ00);		//8 bits, No parity, 2 stop bits
+	return;
+	return;
 }
 int main(void)
 {
