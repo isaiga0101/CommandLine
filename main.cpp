9406f055841e1d9fdd5c361f49fe2496772c520e
isaiga0101 <gayfieldfamily@hotmail.com>
2016-03-03 20:37:09 -0800

    Start td function.

--- main.cpp
+++ main.cpp
@@ -68,6 +68,27 @@ void serial328::init_tx(unsigned char n_clock)
 		//UCSR0C |= (1<<UCSZ01) | (1<<UCSZ00);		//8 bits, No parity, 2 stop bits
 		return;
 }
+//----------------------------------------------------------------------------
+// Transmits x in decimal. Transmits it as 3 ascii characters. Number must be less than 200.
+void serial328::td(unsigned char x)
+{
+	 if (x>=200)  {tx('2'); x-=200;}
+else if (x>=100) {tx('1'); x-=100;}
+else			 {tx('0');		  }
+	
+	 if (x>=90) {tx('9'); x-=90;}
+else if (x>=80) {tx('8'); x-=80;}
+else if (x>=70) {tx('7'); x-=70;}
+else if (x>=60) {tx('6'); x-=60;}
+else if (x>=50) {tx('5'); x-=50;}
+else if (x>=80) {tx('4'); x-=40;}
+else if (x>=70) {tx('3'); x-=30;}
+else if (x>=60) {tx('2'); x-=20;}
+else if (x>=50) {tx('1'); x-=10;}
+else            {tx('0');       }
+	
+	 if (x>=9 ) {tx('9'); x-=9 ;}
+}
 
 
 int main(void)
