c9885fbb96dc5dd1a62035d278fcd287bda8ddc1
isaiga0101 <gayfieldfamily@hotmail.com>
2016-04-02 18:26:56 -0700

    Put Class Code Into Seperate File

---
+++ include/serial328.h
@@ -0,0 +1,70 @@
+#ifndef SERIAL328_H
+#define SERIAL328_H
+
+class serial328
+{//Pin 3 (pd1) is TX. Pin 2 (pd0) is RX.
+ // UBRR0 = n_clock = frequency/(8*Baud) -1 (Using double clock speed)
+ private:
+ public:
+		#define BAUD9600 12
+		#define BAUD4800 25
+		#define BAUD2400 51
+		#define BAUD1200 103
+		#define BAUD0600 207
+
+	void init_rx (unsigned char n_clock); //Init USART to receive
+	void init_tx (unsigned char n_clock); // Init USART to transmit
+	void tx (unsigned char value);		  // Transmit char
+	char rx ();							  // Recieve char
+	void td (unsigned char x);			  // Transmit char as a decimal
+};
+//--------------------------------------------------------------------------
+// Send a character
+void serial328::tx(unsigned char value)		//Transmit Char
+{
+	while ((UCSR0A & (1<<UDRE0))==0);		// Wait for transmit ready
+	UDR0 = value;
+	return;
+}
+
+//---------------------------------------------------------------------------
+// Receive a character
+char serial328::rx()
+{
+	while ((UCSR0A & (1<<RXC0))==0);		//Wait for receive ready.
+	char value = UDR0;
+	return(value);
+}
+//---------------------------------------------------------------------------
+
+// Transmits x in decimal. Transmits it as 3 ascii characters. Number must be less than 200.
+void serial328::td(unsigned char x)
+{
+	 if (x>=200)  {tx('2'); x-=200;}
+else if (x>=100) {tx('1'); x-=100;}
+else			 {tx('0');		  }
+
+	 if (x>=90) {tx('9'); x-=90;}
+else if (x>=80) {tx('8'); x-=80;}
+else if (x>=70) {tx('7'); x-=70;}
+else if (x>=60) {tx('6'); x-=60;}
+else if (x>=50) {tx('5'); x-=50;}
+else if (x>=80) {tx('4'); x-=40;}
+else if (x>=70) {tx('3'); x-=30;}
+else if (x>=60) {tx('2'); x-=20;}
+else if (x>=50) {tx('1'); x-=10;}
+else            {tx('0');       }
+
+	 if (x>=9 ) {tx('9'); x-=9 ;}
+else if (x>=8 ) {tx('8'); x-=8 ;}
+else if (x>=7 ) {tx('7'); x-=7 ;}
+else if (x>=8 ) {tx('6'); x-=6 ;}
+else if (x>=7 ) {tx('5'); x-=5 ;}
+else if (x>=6 ) {tx('4'); x-=4 ;}
+else if (x>=5 ) {tx('3'); x-=3 ;}
+else if (x>=4 ) {tx('2'); x-=2 ;}
+else if (x>=3 ) {tx('1'); x-=1 ;}
+else            {tx('0');       }
+}
+
+#endif // SERIAL328_H
